61b0667006a30660e7f50dc9384a7863d31ee5db
Lesley <lesleyperea@hotmail.com>
2019-08-29 16:44:52 -0600

    23 Fat arrow parte 2

--- Fatarrow.js
+++ Fatarrow.js
@@ -1,1 +1,11 @@
-var inc = function (x) { return x + 1; };
+function Person(age) {
+    this.age = age;
+    // ejecutandola en el navegador el this es window
+    // ya que es quien hace la llamada
+    this.growOld = function () {
+        this.age++;
+    };
+}
+var person = new Person(1);
+setTimeout(person.growOld, 1000); // debería incrementar 1 + 1 = 2
+setTimeout(function () { console.log(person.age); }, 2000); // Devuelve 1, debería ser 2
