acfc9014f7865fb573bbd47213ac5f4405dc5a49
Lesley <lesleyperea@hotmail.com>
2019-08-29 16:49:18 -0600

    26 Fat arrow parte 5

--- Fatarrow.ts
+++ Fatarrow.ts
@@ -7,4 +7,8 @@ class Person {
 var person = new Person(1);
 setTimeout(person.growOld,1000);
 setTimeout(function() { console.log(person.age); },2000); // 2
-    
+
+// Se iguala la función de la clase a una variable de otra clase
+var growOld = person.growOld;
+// más adelante se llama
+growOld();
