51a53cf6833dff59c31283f57c8974334b588417
Lesley <lesleyperea@hotmail.com>
2019-08-29 11:00:12 -0600

    9 Funciones parte 3

--- Funciones.js
+++ Funciones.js
@@ -1,4 +1,12 @@
-var _this = this;
-setTimeout(function () {
-    console.log(_this); // Elemento que contiene esta función
-}, 2000);
+function padLeft(value, padding) {
+    if (typeof padding === "number") {
+        return Array(padding + 1).join(" ") + value;
+    }
+    if (typeof padding === "string") {
+        return Array(padding.length + 1).join(" ") + value;
+    }
+    throw new Error("Expected String or number, got '" + padding + "'");
+}
+console.log(padLeft("hello", "aaa")); //Ejemplo de funcion con texto -> funciona
+console.log(padLeft("hello", 5)); //Ejemplo de funcion con numero ->funciona
+console.log(padLeft("hello", true)); //Ejemplo de funcion con texto -> No funciona
--- Funciones.ts
+++ Funciones.ts
@@ -1,3 +1,16 @@
-setTimeout(() => {
-    console.log(this);// Elemento que contiene esta función
-},2000);
+function padLeft(value: string, padding: string | number){
+    if(typeof padding === "number"){
+        return Array(padding + 1).join(" ") + value;
+    }
+
+    if(typeof padding ==="string"){
+        return Array(padding.length + 1).join(" ")+ value;
+    }
+
+    throw new Error(`Expected String or number, got '${padding}'`);
+}
+
+console.log(padLeft("hello", "aaa")); //Ejemplo de funcion con texto -> funciona
+console.log(padLeft("hello", 5)); //Ejemplo de funcion con numero ->funciona
+console.log(padLeft("hello",true)); //Ejemplo de funcion con texto -> No funciona
+
