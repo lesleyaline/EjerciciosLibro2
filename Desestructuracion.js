f9f6a6588f622457306945ac56dbf6e388cb568c
Lesley <lesleyperea@hotmail.com>
2019-08-29 16:56:50 -0600

    31 Desestructuracion de arrays

--- Desestructuracion.js
+++ Desestructuracion.js
@@ -1,4 +1,4 @@
-var obj = { x: 1, y: 2, z: 3 };
-console.log(obj.x); // 1
-var x = obj.x, y = obj.y, z = obj.z;
+var array = [1, 2, 3];
+console.log(array[0]); // 1
+var x = array[0], y = array[1], z = array[2];
 console.log(x); // 1
--- Desestructuracion.ts
+++ Desestructuracion.ts
@@ -1,5 +1,5 @@
-var obj = {x: 1, y: 2, z: 3};
-console.log(obj.x); // 1
+var array = [1, 2, 3];
+console.log(array[0]); // 1
 
-var {x, y, z} = obj;
+var [x, y, z] = array;
 console.log(x); // 1
