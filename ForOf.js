3ac21add8b0e00b9eac85582673e6f4bdf02fca4
Lesley <lesleyperea@hotmail.com>
2019-08-29 10:23:23 -0600

    4 For of

---
+++ ForOf.js
@@ -0,0 +1,5 @@
+var list = ["a", "b", "c"];
+for (var _i = 0, list_1 = list; _i < list_1.length; _i++) {
+    var b = list_1[_i];
+    console.log(b); //a, b, c
+}
---
+++ ForOf.ts
@@ -0,0 +1,5 @@
+let list = ["a", "b", "c"];
+
+for (let b of list){
+    console.log(b); //a, b, c
+}
