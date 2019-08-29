e6b4024a3e1cf7be98006c80582d81957f46360f
Lesley <lesleyperea@hotmail.com>
2019-08-29 16:32:57 -0600

    20 TypeAlias parte 3

--- TypeAlias.js
+++ TypeAlias.js
@@ -1,8 +1,8 @@
-function getName(n) {
-    if (typeof n === "string") {
-        return n;
-    }
-    else {
-        return n();
-    }
-}
+// FOO
+var FooIdBrand;
+(function (FooIdBrand) {
+})(FooIdBrand || (FooIdBrand = {}));
+// BAR
+var BarIdBrand;
+(function (BarIdBrand) {
+})(BarIdBrand || (BarIdBrand = {}));
--- TypeAlias.ts
+++ TypeAlias.ts
@@ -1,20 +1,8 @@
-type Name = string;
-type NameResolver = () => string;
-type NameOrResolver = Name | NameResolver;
-function getName(n: NameOrResolver): Name {
-    if (typeof n === "string") {
-        return n;
-    }
-    else {
-        return n();
-    }
-}
+// FOO
+enum FooIdBrand {}
+type FooId = FooIdBrand & string;
+// BAR
+enum BarIdBrand {}
+type BarId = BarIdBrand & string;
 
-type Container<T> = { value: T };
-
-type Tree<T> = {
-    value: T;
-    left: Tree<T>;
-    right: Tree<T>;
-}
     
