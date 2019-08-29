c181295990b2461d1492ad284a57f2f9974c53f3
Lesley <lesleyperea@hotmail.com>
2019-08-29 16:56:07 -0600

    30 Desestructuracion de objetos

---
+++ Desestructuracion.js
@@ -0,0 +1,4 @@
+var obj = { x: 1, y: 2, z: 3 };
+console.log(obj.x); // 1
+var x = obj.x, y = obj.y, z = obj.z;
+console.log(x); // 1
---
+++ Desestructuracion.ts
@@ -0,0 +1,5 @@
+var obj = {x: 1, y: 2, z: 3};
+console.log(obj.x); // 1
+
+var {x, y, z} = obj;
+console.log(x); // 1
