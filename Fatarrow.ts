adaf31e1e453feef2bfc8e81426dd298e0902562
Lesley <lesleyperea@hotmail.com>
2019-08-29 16:47:49 -0600

    25 Fat arrow parte 4

--- Fatarrow.ts
+++ Fatarrow.ts
@@ -1,10 +1,10 @@
-function Person(age) {
-    this.age = age
-    // aquí el this es el objeto y no quien hace la llamada
-    this.growOld = () => {
+class Person {
+    constructor(public age:number) {}
+    growOld = () => {
         this.age++;
     }
 }
 var person = new Person(1);
 setTimeout(person.growOld,1000);
-setTimeout(function() { console.log(person.age); },2000); // devuelve 2
+setTimeout(function() { console.log(person.age); },2000); // 2
+    
