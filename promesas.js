bc21f37e55c77c91ae94b7021dc36fdd6ab1ec26
Lesley <lesleyperea@hotmail.com>
2019-08-30 10:15:35 -0600

    35 promesas ejemplo de llamada a catch

--- promesas.js
+++ promesas.js
@@ -1,9 +1,9 @@
 var promise = new Promise(function (resolve, reject) {
-    resolve(123);
+    reject(new Error("Algo malo a pasado"));
 });
 promise.then(function (res) {
-    console.log('I get called:', res === 123); //Devuelve:true
+    //this is never called
 });
 promise["catch"](function (err) {
-    //nunca es utilizado
+    console.log('Tengo una llamada: ', err.message); //Tengo una llamada algo malo a pasado.
 });
--- promesas.ts
+++ promesas.ts
@@ -1,10 +1,11 @@
-const promise = new Promise((resolve, reject) => {
-    resolve(123);
-});
-promise.then((res) => {
-    console.log('I get called:', res ===123); //Devuelve:true
+const promise = new Promise((resolve, reject)=>{
+    reject(new Error("Algo malo a pasado"));
 });
 
-promise.catch((err) =>{
- //nunca es utilizado
+promise.then((res) =>{
+    //this is never called
+});
+
+promise.catch((err)=>{
+    console.log('Tengo una llamada: ',err.message); //Tengo una llamada algo malo a pasado.
 });
