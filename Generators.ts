6180d0960b03f18666e3ba545c035dd99838881f
Lesley <lesleyperea@hotmail.com>
2019-08-30 11:18:32 -0600

    44 Generators parte 2

--- Generators.ts
+++ Generators.ts
@@ -1,11 +1,10 @@
-function* infiniteSequence() {
-    var i = 0;
-    while(true) {
-        yield i++;
-    }
+function* idMaker() {
+    let index = 0;
+    while (index < 3)
+        yield index++;
 }
-var iterator = infiniteSequence();
-    while (true) {
-        console.log(iterator.next()); 
-    }
-    
+let gen = idMaker();    
+console.log(gen.next()); // { value: 0, done: false }
+console.log(gen.next()); // { value: 1, done: false }
+console.log(gen.next()); // { value: 2, done: false }
+console.log(gen.next()); // { value: undefined, done: true }
