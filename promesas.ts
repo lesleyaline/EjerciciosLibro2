f54346c4452e8bcbfb4a80e979ed795eae41ccf6
Lesley <lesleyperea@hotmail.com>
2019-08-30 10:23:42 -0600

    36 cadenas de promesas

--- promesas.ts
+++ promesas.ts
@@ -1,11 +1,16 @@
-const promise = new Promise((resolve, reject)=>{
-    reject(new Error("Algo malo a pasado"));
-});
 
-promise.then((res) =>{
-    //this is never called
-});
-
-promise.catch((err)=>{
-    console.log('Tengo una llamada: ',err.message); //Tengo una llamada algo malo a pasado.
-});
+//cadenas de promesas o chain-ability of Promises
+Promise.resolve(123)
+    .then((res) =>{
+        console.log(res);//123
+        return 456;
+    })
+    .then((res) => {
+        console.log(res);//456
+        return Promise.resolve(123);
+    })
+    .then((res) => {
+        console.log(res); //123
+value
+    return Promise.resolve(123);    
+})
