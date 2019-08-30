7a684d1ee34ab3edd7105945eacb6a5e32cb1ce0
Lesley <lesleyperea@hotmail.com>
2019-08-30 11:37:13 -0600

    51 Async Await

---
+++ AsyncAwait.ts
@@ -0,0 +1,9 @@
+async function foo() {
+    try {
+        var val = await getMeAPromise();
+        console.log(val);
+    }
+    catch(err) {
+        console.log('Error: ', err.message);
+    }
+}
