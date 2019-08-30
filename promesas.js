dd66ea9c4fef3d25cb59ed1cf150dfafe8fc8c1b
Lesley <lesleyperea@hotmail.com>
2019-08-30 10:36:54 -0600

    38 cadena de promesas parte 3

--- promesas.js
+++ promesas.js
@@ -2,14 +2,10 @@ Promise.reject(new Error('Something bad happened'))
     .then(function (res) {
     console.log(res); //not called
     return 456;
-})
-    .then(function (res) {
-    console.log(res); //not called
-    return Promise.resolve(123);
-})
-    .then(function (res) {
-    console.log(res); //not called
-    return Promise.resolve(123);
 })["catch"](function (err) {
     console.log(err.message); //something bad happened
+    return Promise.resolve(123);
+})
+    .then(function (res) {
+    console.log(res); //123
 });
--- promesas.ts
+++ promesas.ts
@@ -3,14 +3,10 @@ Promise.reject(new Error('Something bad happened'))
         console.log(res); //not called
         return 456;
     })
-    .then((res) =>{
-        console.log(res); //not called
-        return Promise.resolve(123);
-    })
-    .then((res) => {
-        console.log(res);//not called
-        return Promise.resolve(123);
-    })
     .catch((err) =>{
         console.log(err.message); //something bad happened
-    });
+        return Promise.resolve(123);
+    })
+    .then((res) =>{
+        console.log(res); //123
+    })
