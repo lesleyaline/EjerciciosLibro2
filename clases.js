66e0ae283ee185f7a84119382d519f6e333b2733
Lesley <lesleyperea@hotmail.com>
2019-08-30 11:51:19 -0600

    55 clases parte 3

--- clases.js
+++ clases.js
@@ -1,10 +1,41 @@
-var Startup = /** @class */ (function () {
-    function Startup() {
+var Calculo = /** @class */ (function () {
+    function Calculo(x, y) {
+        this.x = 0;
+        this.y = 0;
+        this.setX(x);
+        this.setY(y);
     }
-    Startup.main = function () {
-        console.log('Hola mundo');
-        return 0;
+    Calculo.prototype.setX = function (x) {
+        this.x = x;
     };
-    return Startup;
+    Calculo.prototype.setY = function (y) {
+        this.y = y;
+    };
+    Calculo.prototype.getX = function () {
+        return this.x;
+    };
+    Calculo.prototype.getY = function () {
+        return this.y;
+    };
+    Calculo.prototype.sumar = function () {
+        return (this.getX() + this.getY());
+    };
+    Calculo.prototype.restar = function () {
+        return (this.mayor() - this.menor());
+    };
+    Calculo.prototype.menor = function () {
+        if (this.getX() >= this.getY()) {
+            return this.getY();
+        }
+        return this.getX();
+    };
+    Calculo.prototype.mayor = function () {
+        if (this.getX() >= this.getY()) {
+            return this.getX();
+        }
+        return this.getY();
+    };
+    return Calculo;
 }());
-Startup.main();
+var calculo = new Calculo(30, 10);
+console.log(calculo.restar());
--- clases.ts
+++ clases.ts
@@ -1,7 +1,41 @@
-class Startup {
-    public static main(): number {
-        console.log('Hola mundo');
-        return 0;
+class Calculo{
+    private x: number =0;
+    private y: number=0;
+
+    constructor (x:number, y:number){
+        this.setX(x);
+        this.setY(y);
+    }
+    public setX(x:number):void{
+        this.x=x;
+    }
+    public setY(y:number):void{
+        this.y=y;
+    }
+    public getX():number{
+        return this.x;
+    }
+    public getY():number{
+        return this.y;
+    }
+    public sumar():number{
+        return(this.getX() + this.getY());
+    }
+    public restar():number{
+        return (this.mayor() - this.menor());
+    }
+    public menor():number{
+        if(this.getX() >= this.getY()){
+            return this.getY();
+        }
+        return this.getX();
+    }
+    public mayor():number{
+        if(this.getX()>= this.getY()){
+            return this.getX();
+        }
+        return this.getY();
     }
 }
-Startup.main();
+let calculo = new Calculo(30,10);
+console.log(calculo.restar());
