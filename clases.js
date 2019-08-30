a89062e8f639743b10e7d4b1bfa630c8ed25c4df
Lesley <lesleyperea@hotmail.com>
2019-08-30 11:59:09 -0600

    56 clases parte 4

--- clases.js
+++ clases.js
@@ -1,41 +1,16 @@
-var Calculo = /** @class */ (function () {
-    function Calculo(x, y) {
-        this.x = 0;
-        this.y = 0;
-        this.setX(x);
-        this.setY(y);
+var Greeter = /** @class */ (function () {
+    function Greeter(message) {
+        this.greeting = message;
     }
-    Calculo.prototype.setX = function (x) {
-        this.x = x;
+    Greeter.prototype.greet = function () {
+        return "Hello, " + this.greeting;
     };
-    Calculo.prototype.setY = function (y) {
-        this.y = y;
-    };
-    Calculo.prototype.getX = function () {
-        return this.x;
-    };
-    Calculo.prototype.getY = function () {
-        return this.y;
-    };
-    Calculo.prototype.sumar = function () {
-        return (this.getX() + this.getY());
-    };
-    Calculo.prototype.restar = function () {
-        return (this.mayor() - this.menor());
-    };
-    Calculo.prototype.menor = function () {
-        if (this.getX() >= this.getY()) {
-            return this.getY();
-        }
-        return this.getX();
-    };
-    Calculo.prototype.mayor = function () {
-        if (this.getX() >= this.getY()) {
-            return this.getX();
-        }
-        return this.getY();
-    };
-    return Calculo;
+    return Greeter;
 }());
-var calculo = new Calculo(30, 10);
-console.log(calculo.restar());
+var greeter = new Greeter("world");
+var button = document.createElement('button');
+button.textContent = "Say Hello";
+button.onclick = function () {
+    alert(greeter.greet());
+};
+document.body.appendChild(button);
--- clases.ts
+++ clases.ts
@@ -1,41 +1,17 @@
-class Calculo{
-    private x: number =0;
-    private y: number=0;
-
-    constructor (x:number, y:number){
-        this.setX(x);
-        this.setY(y);
+class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
     }
-    public setX(x:number):void{
-        this.x=x;
-    }
-    public setY(y:number):void{
-        this.y=y;
-    }
-    public getX():number{
-        return this.x;
-    }
-    public getY():number{
-        return this.y;
-    }
-    public sumar():number{
-        return(this.getX() + this.getY());
-    }
-    public restar():number{
-        return (this.mayor() - this.menor());
-    }
-    public menor():number{
-        if(this.getX() >= this.getY()){
-            return this.getY();
-        }
-        return this.getX();
-    }
-    public mayor():number{
-        if(this.getX()>= this.getY()){
-            return this.getX();
-        }
-        return this.getY();
+    greet() {
+        return "Hello, " + this.greeting;
     }
 }
-let calculo = new Calculo(30,10);
-console.log(calculo.restar());
+let greeter = new Greeter("world");
+let button = document.createElement('button');
+button.textContent = "Say Hello";
+button.onclick = function() {
+    alert(greeter.greet());
+}
+document.body.appendChild(button);
+    
