2193d68d38253ce1afc8a65c557dc1fc95cdf001
Lesley <lesleyperea@hotmail.com>
2019-08-31 18:57:46 -0600

    67 class decorator

--- decorators.js
+++ decorators.js
@@ -4,42 +4,26 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __param = (this && this.__param) || function (paramIndex, decorator) {
-    return function (target, key) { decorator(target, key, paramIndex); }
-};
-function log(constructor) {
-    console.log('Registered Class: ' + constructor['name'] + ' at ' + Date.now());
+function ClassDecoratorParams(param1, param2) {
+    return function (target // The class the decorator is declared on
+    ) {
+        console.log("ClassDecoratorParams(" + param1 + ", '" + param2 + "') called on:", target);
+    };
 }
-function logm(target, propertyKey, descriptor) {
-    console.log('Registered Method: ' + propertyKey.toString() + ' at ' + Date.now());
-}
-function logparam(target, propertyKey, parameterIndex) {
-    console.log('Registered Parameter: ' + propertyKey.toString() + ' - ' + parameterIndex + ' at ' + Date.now());
-}
-// Así se utiliza un decorador
-var MyClass = /** @class */ (function () {
-    function MyClass() {
-        this.name = 'name';
-        console.log('constructor');
+var ClassDecoratorParamsExample = /** @class */ (function () {
+    function ClassDecoratorParamsExample() {
     }
-    MyClass.prototype.myMethod = function () {
-        console.log('method');
-    };
-    MyClass.prototype.myMethod2 = function (param1, param2) {
-        console.log('method2');
-    };
-    __decorate([
-        logm
-    ], MyClass.prototype, "myMethod");
-    __decorate([
-        logm,
-        __param(1, logparam)
-    ], MyClass.prototype, "myMethod2");
-    MyClass = __decorate([
-        log
-    ], MyClass);
-    return MyClass;
+    ClassDecoratorParamsExample = __decorate([
+        ClassDecoratorParams(1, "a"),
+        ClassDecoratorParams(2, "b")
+    ], ClassDecoratorParamsExample);
+    return ClassDecoratorParamsExample;
 }());
-var myClass = new MyClass();
-myClass.myMethod();
-myClass.myMethod2(1, false);
+ClassDecoratorParams(2, 'b');
+called;
+on: function ClassDecoratorParamsExample() {
+}
+ClassDecoratorParams(1, 'a');
+called;
+on: function ClassDecoratorParamsExample() {
+}
--- decorators.ts
+++ decorators.ts
@@ -1,28 +1,16 @@
-function log(constructor: Function): void{
-    console.log('Registered Class: '+ constructor['name'] + ' at ' + Date.now());
-}
-function logm<T>(target: Object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<T>): void {
-    console.log('Registered Method: ' + propertyKey.toString() + ' at ' + Date.now());
-}
-function logparam (target: Object, propertyKey: string | symbol, parameterIndex: number): void {
-    console.log('Registered Parameter: ' + propertyKey.toString() + ' - ' + parameterIndex + ' at ' + Date.now());
-}
-// Así se utiliza un decorador
-@log
-class MyClass {
-    public name: string = 'name';
-    constructor() {
-        console.log('constructor');
-    }
-    @logm
-    public myMethod() {
-        console.log('method')
-    }
-    @logm
-    public myMethod2(param1: number, @logparam param2: boolean) {
-        console.log('method2')
+function ClassDecoratorParams(param1: number, param2: string) {
+    return function(
+        target: Function // The class the decorator is declared on
+        ) {
+        console.log("ClassDecoratorParams(" + param1 + ", '" + param2 + "') called on:", target);
     }
 }
-var myClass = new MyClass();
-myClass.myMethod();
-myClass.myMethod2(1, false);
+@ClassDecoratorParams(1, "a")
+@ClassDecoratorParams(2, "b")
+class ClassDecoratorParamsExample {
+}
+ClassDecoratorParams(2, 'b') called on: function ClassDecoratorParamsExample() {
+}
+ClassDecoratorParams(1, 'a') called on: function ClassDecoratorParamsExample() {
+}
+    
