5e9907c647e2bba744864f8274be09b9f1731a96
Lesley <lesleyperea@hotmail.com>
2019-08-31 18:39:38 -0600

    65 interfaces

--- mixin.js
+++ mixin.js
@@ -1,45 +1,7 @@
-var Disposable = /** @class */ (function () {
-    function Disposable() {
+var test = /** @class */ (function () {
+    function test(b, c) {
+        this.b = b;
+        this.c = c;
     }
-    Disposable.prototype.dispose = function () {
-        this.isDisposed = true;
-    };
-    return Disposable;
+    return test;
 }());
-var Activatable = /** @class */ (function () {
-    function Activatable() {
-    }
-    Activatable.prototype.activate = function () {
-        this.isActive = true;
-    };
-    Activatable.prototype.deactivate = function () {
-        this.isActive = false;
-    };
-    return Activatable;
-}());
-var SmartObject = /** @class */ (function () {
-    //Este constructor lo que hara sera que mostrará por pantalla los estados isactive y isdisposed cada 500 ms
-    function SmartObject() {
-        var _this = this;
-        //Disposable
-        this.isDisposed = false;
-        //Activatable
-        this.isActive = false;
-        setInterval(function () { return console.log(_this.isActive + " : " + _this.isDisposed); }, 500);
-    }
-    SmartObject.prototype.interact = function () {
-        this.activate();
-    };
-    return SmartObject;
-}());
-applyMixins(SmartObject, [Disposable, Activatable]);
-var smartObj = new SmartObject();
-//esto generará una interactuacion cada segundo cambiando el estado de false false a true true la primera vez, luego como la variable ya valdría true true simplemente se dedicará a mostrarla cada segundo
-setTimeout(function () { return smartObj.interact(); }, 1000);
-function applyMixins(derivedCtor, baseCtors) {
-    baseCtors.forEach(function (baseCtor) {
-        Object.getOwnPropertyNames(baseCtor.prototype).forEach(function (name) {
-            derivedCtor.prototype[name] = baseCtor.prototype[name];
-        });
-    });
-}
--- mixin.ts
+++ mixin.ts
@@ -1,45 +1,15 @@
-class Disposable{
-    isDisposed: boolean;
-    dispose(){
-        this.isDisposed=true;
+interface a {
+    b: number;
+}
+interface b extends a {
+    c: string;
+}
+class test implements b {
+    b: number;
+    c: string;
+    constructor (b: number, c: string) {
+        this.b = b;
+        this.c = c;
     }
 }
-
-class Activatable{
-    isActive:boolean;
-    activate(){
-        this.isActive=true;
-    }
-    deactivate(){
-        this.isActive=false;
-    }
-}
-
-class SmartObject implements Disposable, Activatable{
-    //Este constructor lo que hara sera que mostrará por pantalla los estados isactive y isdisposed cada 500 ms
-    constructor(){
-        setInterval(()=> console.log(this.isActive + " : " + this.isDisposed),500);
-    }
-    interact(){
-        this.activate();
-    }
-    //Disposable
-    isDisposed: boolean =false;
-    dispose:()=> void;
-    //Activatable
-    isActive: boolean=false;
-    activate:()=>void;
-    deactivate:()=>void;
-}
-applyMixins(SmartObject, [Disposable, Activatable]);
-let smartObj = new SmartObject();
-//esto generará una interactuacion cada segundo cambiando el estado de false false a true true la primera vez, luego como la variable ya valdría true true simplemente se dedicará a mostrarla cada segundo
-setTimeout(()=> smartObj.interact(),1000);
-
-function applyMixins (derivedCtor: any, baseCtors:any[]){
-    baseCtors.forEach(baseCtor=>{
-        Object.getOwnPropertyNames(baseCtor.prototype).forEach(name=>{
-            derivedCtor.prototype[name] = baseCtor.prototype[name];
-        });
-    });
-}
+    
